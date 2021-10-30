b5aaa6048f00e32c2688e649064f8bab69c6005b
raffael marinho <silvamarinho10@gmail.com>
2021-10-29 21:56:37 -0300

    feat: requeriment 6 done

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 function SolarSystem(prop) {
   const { children } = prop;
-  return <div data-testid="solar-system">{children}</div>;
+  return (
+    <div data-testid="solar-system">
+      {children}
+      {planets.map((planet, index) => (
+        <PlanetCard
+          key={ index }
+          planetName={ planet.name }
+          planetImage={ planet.image }
+        />
+      ))}
+      planetImage
+    </div>
+  );
 }
 
 export default SolarSystem;
